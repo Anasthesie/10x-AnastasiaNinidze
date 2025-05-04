b07f06e721e6661fefc3337b57a9e40226d20f08
Anasthesie <118045856+Anasthesie@users.noreply.github.com>
2025-05-05 01:21:00 +0400

    started working on js!

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,4 +1,11 @@
 const randomNums = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 141, 516, 17, 18, 19, 20,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
+function getNum() {
+  const randomIndex = Math.floor(Math.random() * randomNums.length);
+  const takenNum = randomNums[randomIndex];
+  return takenNum;
+}
 
+const numberToGuess = getNum();
+console.log(numberToGuess);
